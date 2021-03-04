84b88a4fbd0e11dd33a7a444a62db94ccc003da4
jeffreytse <jeffreytse.mail@gmail.com>
2021-03-04 10:42:02 +0800

    chore: revert the default bindings of arrow up/down (#15)

--- zsh-vi-mode.zsh
+++ zsh-vi-mode.zsh
@@ -2193,10 +2193,6 @@ function zvm_init() {
   zvm_bindkey viins '^S' history-incremental-search-forward
   zvm_bindkey viins '^P' up-line-or-history
   zvm_bindkey viins '^N' down-line-or-history
-  zvm_bindkey viins '^[[A' history-beginning-search-backward
-  zvm_bindkey viins '^[[B' history-beginning-search-forward
-  zvm_bindkey vicmd '^[[A' history-beginning-search-backward
-  zvm_bindkey vicmd '^[[B' history-beginning-search-forward
 
   # Insert mode
   zvm_bindkey vicmd 'i'  zvm_enter_insert_mode
