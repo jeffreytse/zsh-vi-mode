jeffreytse/zsh-vi-mode

commit ef1fdbee77ecdf38e3205a435b714ee5f658edb2

feat: syntax highlighting with external editor (#109)
* feat: add .zsh extension on editer mode

* perf: syntax highlighting with external editor

It follows the file style of zle built-in widget `edit-command-line`, and it
makes the external editor highlight the syntax correctly.

Co-authored-by: JT <jeffreytse.mail@gmail.com>

zsh-vi-mode.zsh
@@ -596,7 +596,7 @@ function zvm_escape_non_printed_characters() {
       str="${str}^${c}"
     elif [[ "$c" == '' ]]; then
       str="${str}^?"
-    elif [[ "$c" == ' ' ]]; then
+    elif [[ "$c" == '' ]]; then
       str="${str}^@"
     else
       str="${str}${c}"
@@ -1703,7 +1703,7 @@ function zvm_range_handler() {
 # Edit command line in EDITOR
 function zvm_vi_edit_command_line() {
   # Create a temporary file and save the BUFFER to it
-  local tmp_file=$(mktemp ${ZVM_TMPDIR}zvm.XXXXXX)
+  local tmp_file=$(mktemp ${ZVM_TMPDIR}zshXXXXXX)
   echo "$BUFFER" > "$tmp_file"
 
   # Edit the file with the specific editor, in case of

commit 47bdd0b16a611b9f1dbcd0fc46431f301741c8cf

fix: escape the special control character `^@`
It fixes a wrong change on escaping the control character `^@`,
and this bug is introduced by the PR #109.

zsh-vi-mode.zsh
@@ -596,7 +596,7 @@ function zvm_escape_non_printed_characters() {
       str="${str}^${c}"
     elif [[ "$c" == '' ]]; then
       str="${str}^?"
-    elif [[ "$c" == '' ]]; then
+    elif [[ "$c" == ' ' ]]; then
       str="${str}^@"
     else
       str="${str}${c}"