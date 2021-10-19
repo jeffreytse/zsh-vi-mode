0c6f34a84fa4d5781c36135b3a7fddb426473c1b
jeffreytse <jeffreytse.mail@gmail.com>
2021-10-19 22:55:13 +0800

    feat: support to custom user default cursor (#131)
    When you enter a CLI program from the prompt, this plugin will
    restore the cursor to terminal's default, this feature allows you
    to custom user default cursor instead of terminal's default.

--- zsh-vi-mode.zsh
+++ zsh-vi-mode.zsh
@@ -3020,13 +3020,13 @@ function zvm_cursor_style() {
     # 256 colors the same way xterm does.
     xterm*|rxvt*|screen*|tmux*|konsole*|alacritty*|st*)
       case $style in
-        $ZVM_CURSOR_USER_DEFAULT) style='\e[0 q';;
         $ZVM_CURSOR_BLOCK) style='\e[2 q';;
         $ZVM_CURSOR_UNDERLINE) style='\e[4 q';;
         $ZVM_CURSOR_BEAM) style='\e[6 q';;
         $ZVM_CURSOR_BLINKING_BLOCK) style='\e[1 q';;
         $ZVM_CURSOR_BLINKING_UNDERLINE) style='\e[3 q';;
         $ZVM_CURSOR_BLINKING_BEAM) style='\e[5 q';;
+        $ZVM_CURSOR_USER_DEFAULT) style='\e[0 q';;
       esac
       ;;
     *) style='\e[0 q';;
